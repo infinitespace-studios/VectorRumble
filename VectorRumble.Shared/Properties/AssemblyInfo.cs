886715495582ba488a16be455a8b02745968f364
Dean Ellis <dellis1972@googlemail.com>
2019-10-11 10:10:05 +0100

    Unify the AssemblyInfo.cs into one file.

--- VectorRumble.Shared/Properties/AssemblyInfo.cs
+++ VectorRumble.Shared/Properties/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+#if __ANDROID__
+using Android.App;
+#endif
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
