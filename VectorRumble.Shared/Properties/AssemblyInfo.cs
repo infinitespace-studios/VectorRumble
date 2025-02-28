6700d81054b629f9b28b7c59564592d19de3da61
Dean Ellis <dellis1972@googlemail.com>
2025-02-28 23:47:25 +0000

    Update DesktopGL to .net 9.0

--- VectorRumble.Shared/Properties/AssemblyInfo.cs
+++ VectorRumble.Shared/Properties/AssemblyInfo.cs
@@ -8,14 +8,14 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vector Rumble")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Infinitespace Studios")]
-[assembly: AssemblyProduct("Vector Rumble")]
-[assembly: AssemblyCopyright("Copyright Infinitespace Studios ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+// [assembly: AssemblyTitle("Vector Rumble")]
+// [assembly: AssemblyDescription("")]
+// [assembly: AssemblyConfiguration("")]
+// [assembly: AssemblyCompany("Infinitespace Studios")]
+// [assembly: AssemblyProduct("Vector Rumble")]
+// [assembly: AssemblyCopyright("Copyright Infinitespace Studios ©  2016")]
+// [assembly: AssemblyTrademark("")]
+// [assembly: AssemblyCulture("")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -27,6 +27,6 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.2")]
-[assembly: AssemblyFileVersion("0.1.0.2")]
-[assembly: ComVisible(false)]
+// [assembly: AssemblyVersion("0.1.0.2")]
+// [assembly: AssemblyFileVersion("0.1.0.2")]
+// [assembly: ComVisible(false)]
